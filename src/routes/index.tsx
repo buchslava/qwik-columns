d001b92160b421fa194809ce946e48de26f19744
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2023-01-15 19:03:41 +0200

    start

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import { Link } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div>ok
+    </div>
+  );
+});
