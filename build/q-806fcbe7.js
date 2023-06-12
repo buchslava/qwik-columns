580649b3980626675e2265545fa3291c62bf012a
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2023-06-12 10:16:58 +0300

    new version

---
+++ build/q-806fcbe7.js
@@ -0,0 +1,1 @@
+import{k as o,a as _,_ as t}from"./q-1f2f0220.js";const r=o(_(()=>t(()=>import("./q-22f0016b.js"),["build/q-22f0016b.js","build/q-1f2f0220.js","build/q-98a6be33.js"]),"s_3sccYCDd1Z0"));export{r as default};
